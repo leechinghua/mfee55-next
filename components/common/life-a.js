4d4d99ffe0b0398736ff44f068cd48761b71c53c
leechinghua <leechenghua@gmail.com>
2024-07-04 09:56:31 +0800

    掛載卸載的使用情境 1

--- components/common/life-a.js
+++ components/common/life-a.js
@@ -1,17 +1,19 @@
-import React, { useEffect, useRef } from "react";
+import  { useEffect, useRef } from "react";
 
 export default function LifeA() {
   const refA = useRef();
   useEffect(() => {
     console.log("LifeA已經掛載---");
     let n = 0;
-    setInterval(() => {
+    // 設定計時器, 同時取得計時器的編號
+    const interval_id = setInterval(() => {
       n++;
       console.log({ n });
       refA.current.innerHTML = n;
     }, 500);
     return () => {
       console.log("LifeA即將卸載===");
+      clearInterval(interval_id); // 取消計時器的功能
     };
   }, []);
   return (

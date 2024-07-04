4aa0b51d2a5709514acbd0c8d27b92d835c93a7c
leechinghua <leechenghua@gmail.com>
2024-07-04 10:47:40 +0800

    update 的狀況

--- components/common/life-a.js
+++ components/common/life-a.js
@@ -1,6 +1,8 @@
 import  { useEffect, useRef } from "react";
 
 export default function LifeA() {
+  const [val, setVal] = useState(0);
+
   const refA = useRef();
   useEffect(() => {
     console.log("LifeA已經掛載---");
@@ -8,7 +10,7 @@ export default function LifeA() {
     // 設定計時器, 同時取得計時器的編號
     const interval_id = setInterval(() => {
       n++;
-      console.log({ n });
+      // console.log({ n });
       refA.current.innerHTML = n;
     }, 500);
     return () => {
@@ -16,10 +18,18 @@ export default function LifeA() {
       clearInterval(interval_id); // 取消計時器的功能
     };
   }, []);
+  useEffect(()=>{
+    console.log("LifeA 已經更新 ...");
+    return ()=>{
+      console.log("LifeA 即將更新 !!!")
+    }
+  }, [val]);
   return (
     <div>
       <h2>Life A</h2>
       <h5 ref={refA}>begin</h5>
+      <div>
+      <button onClick={()=> setVal(val + 1)}>val++</button>{val}</div>
     </div>
   );
 }
